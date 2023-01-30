6d9002c8ccbb749e81f3159781e462e8dd3ed5e9
joshdavid0109 <2222551@slu.edu.ph>
2023-01-30 21:51:13 +0800

    reading csv file, printing dates for first 1000 rows

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,54 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        String line = "";
+        String delimiter = ";";
+        int temp = -1;
+        int i = 1;
+
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(""))
+            BufferedReader br = new BufferedReader(new FileReader("stormwater-pipes_3.csv"));
+            // Reading the file line by line and stopping when it reaches the 1000th line.
+            while (((line = br.readLine()) != null) && i <= 1000) {
+                String [] rowData = line.split(delimiter);
+                String columnHeader = "createdate";
+
+                if (temp == -1) {
+                    temp = findColumn(rowData, columnHeader);;
+                    continue;
+                }
+
+                if (rowData[temp].equals("")) {
+                    System.out.printf("Date for row [%d] " , i);
+                    System.out.println();
+                    i++;
+                    continue;
+                }
+
+                System.out.printf("Date for row [%d] " + rowData[temp].substring(0, rowData[temp].indexOf(' ')), i);
+                System.out.println();
+
+                i++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
+    }
 
-
+    public static int findColumn(String [] rowData, String cHeader) {
+        int x = 0;
+        for (int i = 1; i <= rowData.length; i++) {
+            if (rowData[i].equals(cHeader)){
+                x = i;
+                return x;
+            }
+        }
+        return x;
     }
 }
