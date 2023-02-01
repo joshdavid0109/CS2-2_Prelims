dc46113e3b7b221585a4a0579085ec5cbe70c52f
kiko <2224514@slu.edu.ph>
2023-02-01 17:28:23 +0800

    removed extends

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MainMenu extends Main {
+public class MainMenu {
     private static List<rowData> rowDataList = new ArrayList<>();
     static Scanner kbd = new Scanner(System.in);
 
